ef747de3add96638f5a2354fe80c68b793099ffd
Alldayallan <mail@allanreid.co>
2025-02-19 06:11:32 -0800

    Getting started

--- Flashed/View/ContentView.swift
+++ Flashed/View/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        NavigationStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Flashed")
         }
         .padding()
     }
