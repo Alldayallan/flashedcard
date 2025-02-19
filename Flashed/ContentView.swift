e5912a4445ec96febd8858b442793a3ccdcebe8d
Alldayallan <mail@allanreid.co>
2025-02-19 05:54:59 -0800

    Initial Commit

---
+++ Flashed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashed
+//
+//  Created by Allan Reid on 2/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flashed/FlashedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashedApp.swift
+//  Flashed
+//
+//  Created by Allan Reid on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
