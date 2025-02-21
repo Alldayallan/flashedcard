4a8544587d45bd0552a8750be6764c91e7982180
Alldayallan <mail@allanreid.co>
2025-02-20 19:14:06 -0800

    working on the QuestionModel view

--- Flashed/View/TopicView.swift
+++ Flashed/View/TopicView.swift
@@ -5,6 +5,13 @@ struct TopicView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                
+                Image("imgPeaceful3")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(.rect(cornerRadius: 15))
+                    .shadow(radius:3)
+                
                 Text("TODO...")
             }
             .navigationTitle("Flashed")
