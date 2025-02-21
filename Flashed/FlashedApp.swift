9913a381c701af3ba11b131b7af36425b5ddaf58
Alldayallan <mail@allanreid.co>
2025-02-20 16:30:54 -0800

    TopicView file added

--- Flashed/FlashedApp.swift
+++ Flashed/FlashedApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FlashedApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TopicView()
         }
     }
 }
---
+++ Flashed/View/TopicView.swift
@@ -0,0 +1,17 @@
+
+import SwiftUI
+
+struct TopicView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("TODO...")
+            }
+            .navigationTitle("Flashed")
+        }
+    }
+}
+
+#Preview {
+    TopicView()
+}
