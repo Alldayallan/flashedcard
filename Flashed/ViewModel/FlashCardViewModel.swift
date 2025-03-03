111151effa3d22b3abd0eac025c88b20f02bec25
Alldayallan <mail@allanreid.co>
2025-03-03 10:47:42 -0800

    flashcard view model code added

---
+++ Flashed/ViewModel/FlashCardViewModel.swift
@@ -0,0 +1,34 @@
+
+import Foundation
+
+@Observable
+class FlashCardViewModel {
+    var flashCardModels:  [FlashcardModel] = [
+        .init(
+            topic: "Countries",
+            topicDescription: "Explore geographical knowledge",
+            topicImageName: .bigBen,
+            questionModels: QuestionViewModel.countryQuestionModels
+        ),
+        .init(
+            topic: "Movies",
+            topicDescription: "Test your movie knowledge",
+            topicImageName: .moviesTopic,
+            questionModels: QuestionViewModel.movieTriviaModels
+        ),
+        .init(
+            topic: "Spanish Greetings",
+            topicDescription: "Greet in Spanish",
+            topicImageName: .madrid,
+            questionModels: QuestionViewModel.spanishGreetingsModels
+        ),
+        .init(
+            topic: "Shakespeare",
+            topicDescription: "Shakespeare trivia",
+            topicImageName: .shakespeare,
+            questionModels: QuestionViewModel.shakespeareTriviaQuestions
+        ),
+        
+    ]
+    
+}
